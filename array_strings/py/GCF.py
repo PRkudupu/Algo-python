b28bf59b16d5131bdbc1d094b4e6fcdf2fd1bd4e
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2018-11-08 18:19:55 -0800

    print string program has been modified.

---
+++ array_strings/py/GCF.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Write a function to which would return the greatest common of factor.
+# 
+# <b> Input : 18, 27</b>
+# 
+# <b> return: 9 </b>
+# 
+#         
+
+# #### Loops
+
+# In[12]:
+
+
+def gcd(x,y):
+    if x > y:
+        small=x
+    else:
+        small=x
+    for i in range(1,small+1):
+        if(x % i == 0) and (y % i ==0):
+            gcd=i
+    return gcd
+print(gcd(18,27))
+
+
+# <b>Euclidean Algorithm</b>
+
+# In[13]:
+
+
+def gcd(x,y):
+    while(y):
+        x , y = y,x % y
+    return x
+print(gcd(18,27))
+
+
+# #### Recursion
+
+# In[17]:
+
+
+def gcd(a,b):
+    if(b == 0):
+        return a
+    else:
+        return gcd(b,a%b)
+print(gcd(18,27))
+
+
+# In[ ]:
+
+
+
+
--- array_strings/py/print_part_list.py
+++ array_strings/py/print_part_list.py
@@ -3,15 +3,24 @@
 
 # ### Print parts of the array
 
-# In[5]:
+# In[9]:
 
 
 def print_part(ls):
+    print("ls",ls)
     print("0 to 3",ls[0:3])
     print("Increement by 2",ls[0:3:2])
     print("Increement by 3",ls[0:3:3])
     print("print till the end",ls[0:-1])
-    print("Reverse the list",ls[::-1])
+    print("reverse the list",ls[::-1])
     print("slice",slice(ls[0:2]))
+    print("multiply the list elements ls * 2", ls * 2)
+    print("concate ","concat"  + " test")
 print_part([1,4,5,8,10,12,15])
+
+
+# In[ ]:
+
+
+
 
