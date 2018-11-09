0191340a1ac7477c20edc768bca1e4e2384f4cb4
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2018-11-09 13:29:18 -0800

    return the element in the fibonacci series.

---
+++ array_strings/py/first_last.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# #### Return the first and last element of the list
+# <b>Input </b> : [1,2,10] 
+# 
+# <b>Output </b>: [1,10]
+
+# In[4]:
+
+
+def first_last(ls):
+    return [ls[0],ls[len(ls)-1]]
+print(first_last([1,2,10]))
+
+
+# In[ ]:
+
+
+
+
