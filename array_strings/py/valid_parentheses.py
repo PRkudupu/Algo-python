c9509cd89039191db8143cb543b967d9be092181
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2018-11-13 08:01:30 -0800

    count variable has been removed from valid parenthesis.

--- array_strings/py/valid_parentheses.py
+++ array_strings/py/valid_parentheses.py
@@ -42,21 +42,19 @@
 # In[2]:
 
 
-def is_valid_parentheses(s):
-    stack = []
-    dict = {"]":"[", "}":"{", ")":"("}
-    count =0
-    for char in s:
-        count+=1
-        if char in dict.values():
+def valid_param(input):
+    dic ={']':'[','}':'{',')':'('}
+    stack = [];
+    for char in input:
+        if char in dic.values():
             stack.append(char)
-        elif char in dict.keys():
-            if stack == [] or dict[char] != stack.pop():
+        elif char in dic.keys():
+            if stack == [] or dic[char] != stack.pop():
                 return False
         else:
             return False
     return stack == []
-is_valid_parentheses("{}")
+valid_param("(}")
 
 
 # In[3]:
@@ -88,35 +86,11 @@ is_valid_parentheses("{}")
 # In[5]:
 
 
-def valid_param(input):
-    dic ={']':'[','}':'{',')':'('}
-    stack = [];
-    for char in input:
-        if char in dic.values():
-            stack.append(char)
-        elif char in dic.keys():
-            if stack == [] or dic[char] != stack.pop():
-                return False
-        else:
-            return False
-    return stack == []
-valid_param("(}")
+
 
 
 # In[ ]:
 
 
-stocks=[7,1,5,2,4]
-   
-def buy_sell_once(s):
-    maxprofit=0
-    #first loop
-    for i in range(len(s)-1):
-        for j in range(i+1,len(s)):
-            if s[j] - s[i] > maxprofit:
-                maxprofit=s[j] - s[i]
-    return maxprofit
 
-m=buy_sell_once(stocks)
-print(m)
 
