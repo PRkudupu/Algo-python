4639149e27cf6e1b59336d88fc3edd7f4398c8f6
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2018-11-13 02:46:41 -0800

    Def has been added to prime nos.

--- array_strings/py/check_prime.py
+++ array_strings/py/check_prime.py
@@ -2,6 +2,13 @@
 # coding: utf-8
 
 # #### Validate if a given number is a prime number
+# A prime number is a natural number greater than 1 that cannot be formed by multiplying two smaller natural numbers.
+# 
+# The first few prime numbers are
+# <b> 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29 </b>
+# 
+# Example
+# 
 # <b>Input </b> : 8
 # 
 # <b>Output</b> : False
@@ -11,7 +18,7 @@
 # <b>Output</b> : True
 #         
 
-# In[3]:
+# In[2]:
 
 
 def check_prime(num):
@@ -23,5 +30,3 @@ def check_prime(num):
             return True
 print(check_prime(7))
 
-
-# 
