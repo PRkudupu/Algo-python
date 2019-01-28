fda519f8a23c1e439a261cac0befcbc3eea7ea6e
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2019-01-27 18:22:17 -0800

    Sol hint added in the comment section.

--- array_strings/py/rm_dup_sorted_array.py
+++ array_strings/py/rm_dup_sorted_array.py
@@ -12,6 +12,9 @@
 # Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
 # 
 # It doesn't matter what you leave beyond the returned length.
+# <br><b>sol</b>
+# We use a counter pos: increment the pos if i and i-1 are not equal
+# 
 
 # In[1]:
 
