83da9e195493b5f9695faec19255d110ff035272
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2019-01-28 10:17:12 -0800

    sol added without using range in for loop

--- array_strings/py/first_non_repeating_str.py
+++ array_strings/py/first_non_repeating_str.py
@@ -14,9 +14,27 @@
 # <b>return 2.</b>
 # 
 
-# In[ ]:
+# In[1]:
 
 
+def n_repeat(s):
+    dic ={}
+    for ch in s:
+        if ch in dic.keys():
+            continue
+        else:
+            if s.count(ch) == 1:
+                return ch
+            else:
+                dic[ch]=s.count(ch)
+    return -1     
+print(n_repeat("geekgeeks"))
+
+
+# In[3]:
+
+
+# In this example iteration is done
 def firstUniqChar(s):
         """
         :type s: str
@@ -30,9 +48,36 @@ def firstUniqChar(s):
                 if s.count(s[i]) == 1:
                     return i
                 else:
-                    dic[s[i]]=s[i].count(s[i])
+                    dic[s[i]]=s.count(s[i])
             
         return -1
 
 print(firstUniqChar("saspa"))
+
+
+# In[2]:
+
+
+def f_dup(st):
+    dic = {}
+    for i in range(len(st)):
+        print("st[i]",st[i])
+        print("dic",dic)
+        print("count",st.count(st[i]) )
+        if st[i] in dic.keys():
+            print("inside if")
+            continue
+        else:
+            if st.count(st[i]) == 1:
+                return i
+            else:
+                dic[st[i]]=st.count(st[i])
+    return -1
+print(f_dup("sapsa"))
+
+
+# In[ ]:
+
+
+
 
