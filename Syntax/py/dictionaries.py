fa8e1e4bfa946791a588e653c557806f6ca726f4
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2020-01-13 12:29:54 -0800

    simple dictionaries has been added.

---
+++ Syntax/py/dictionaries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+#Simple dictionary
+dic ={'first':111,'second':2222}
+#Fetch the value for the second
+print(dic['second'])
+#Fetcht the value for first
+print(dic['first'])
+
+
+# In[13]:
+
+
+#Iterate through dic
+for keys in dic:
+    #print only keys
+    print("keys->",keys)
+    #print only values
+    print("values",dic[keys])
+
+
+# In[ ]:
+
+
+
+
