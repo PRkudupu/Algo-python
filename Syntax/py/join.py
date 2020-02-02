b7503715b5d48f477c9a43ba99f9e0feaa8cc3b6
PRATHAP KUDUPU <prathapkudupu@gmail.com>
2020-02-01 17:50:43 -0800

    join syntax has been added.

---
+++ Syntax/py/join.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+ls = ['1','2','3','4']  
+s = "-"
+s = s.join(ls) 
+print(s)
+
+
+# In[7]:
+
+
+ls = ['1','2','3','4']  
+s = ":"
+s = s.join(ls) 
+print(s) 
+
+
+# In[13]:
+
+
+ls = ['1','2','3','4']  
+s = '-'.join(ls) 
+print(s) 
+
+
+# In[6]:
+
+
+#WITH COMMENTS
+ls = ['1','2','3','4']  
+s = "-"
+# joins elements of ls by '-' 
+# and stores in string s 
+s = s.join(ls) 
+
+# join use to join a list of 
+# strings to a separator s 
+print(s) 
+
+
+# In[ ]:
+
+
+
+
